25f43d00dee512ac1aad57bcfcf9fec5a0cfb13d
unknown <sbrnleo13@gmail.com>
2023-11-22 23:07:23 +0700

    project pertamaku

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_5/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("data 1")),
+        body: Center(
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+      ),
+    );
+  }
+}
